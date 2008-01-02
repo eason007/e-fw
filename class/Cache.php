d462ba323c78dc5c27683a542c585642a7ab4af7
czz <czz@94e8bf7a-fd2e-11de-a138-4f08880ddf14>
2008-01-02 07:30:56 +0000

    加入debug模式

--- class/Cache.php
+++ class/Cache.php
@@ -3,9 +3,10 @@ class Class_Cache {
 	public $cacheDir = null;
 	public $cacheType = null;
 	public $cacheTime = 3600;
+	public $hashFile = 2;
 	public $cacheFileExt = ".EFW-Cache";
 	public $isSerialize = false;
-	public $HashFile = 2;
+	public $isDebug = false;
 	
 	public $cacheData = null;
 	
@@ -16,12 +17,17 @@ class Class_Cache {
 			$this->cacheTime 	= $Params["cacheTime"] ? $Params["cacheTime"] : 3600;
 			$this->cacheFileExt	= $Params["cacheFileExt"] ? $Params["cacheFileExt"] : ".EFW-Cache";
 			$this->isSerialize 	= $Params["isSerialize"] ? $Params["isSerialize"] : false;
-			$this->HashFile 	= $Params["HashFile"] ? $Params["HashFile"] : 2;
+			$this->hashFile 	= $Params["hashFile"] ? $Params["hashFile"] : 2;
+			$this->isDebug 		= $Params["isDebug"] ? $Params["isDebug"] : false;
 		}
 	}
 	
 	
 	public function getCache ($cacheID, $unserialize = false) {
+		if ($this->isDebug){
+			return false;
+		}
+		
 		switch ($this->cacheType){
 			case "file":
 				clearstatcache();
@@ -90,11 +96,11 @@ class Class_Cache {
 	
 	
 	private function _getHashPath ($cacheID) {
-		if ($this->HashFile > 0){
+		if ($this->hashFile > 0){
 			$hashName = md5($cacheID);
 			$path = $this->cacheDir.DS;
 			
-			for ($i = 0;$i < $this->HashFile; $i++){
+			for ($i = 0;$i < $this->hashFile; $i++){
 				$path.= $hashName{$i}.DS;
 				
 				if (!is_readable($path)){
