23082c4db0ef86d9d43053c0a10dfea1e29093d3
czz <czz@94e8bf7a-fd2e-11de-a138-4f08880ddf14>
2008-01-09 08:56:37 +0000

    添加注释

--- class/Cache.php
+++ class/Cache.php
@@ -1,13 +1,79 @@
 <?php
+/**
+ * 缓存类
+ * 
+ * 提供页面/数据的缓存服务
+ * 
+ * @package class
+ * @author eason007<eason007@163.com>
+ * @copyright Copyright (c) 2007-2008 eason007<eason007@163.com>
+ * @version 1.0.0.20080108
+ */
+
 class Class_Cache {
+	/**
+	 * 缓存目录
+	 *
+	 * @var string
+	 */
 	public $cacheDir = null;
+	
+	/**
+	 * 缓存方式
+	 * 
+	 * 目前只支持文件形式保存
+	 * 以后计划增加数据库、mmcache方式
+	 * 
+	 * @var string
+	 */
 	public $cacheType = null;
+	
+	/**
+	 * 缓存生命期
+	 *
+	 * @var int
+	 */
 	public $cacheTime = 3600;
+	
+	/**
+	 * 缓存散列层次，仅支持 file 方式
+	 * 0则为不散列，以cachaid为文件名保存在缓存跟目录中
+	 * 大于0则以md5加密cacheid为文件名，保存于多层子目录下
+	 * 最大支持32层子目录散列
+	 *
+	 * @var int
+	 */
 	public $hashFile = 2;
+	
+	/**
+	 * 缓存文件扩展名
+	 *
+	 * @var string
+	 */
 	public $cacheFileExt = ".EFW-Cache";
+	
+	/**
+	 * 是否将缓存数据序列化后保存
+	 *
+	 * @var bool
+	 */
 	public $isSerialize = false;
+	
+	/**
+	 * 是否调试模式。是则缓存不启动
+	 *
+	 * @var bool
+	 */
 	public $isDebug = false;
 	
+	/**
+	 * 缓存内容
+	 * 
+	 * getCache 方法后，缓存数据除通过get方法返回
+	 * 同时亦保存到这里
+	 *
+	 * @var var
+	 */
 	public $cacheData = null;
 	
 	function __construct($Params = null) {
