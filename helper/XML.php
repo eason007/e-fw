db4e000e59fc8c82463b619176da7ff5e8b00b07
czz <czz@94e8bf7a-fd2e-11de-a138-4f08880ddf14>
2008-01-19 15:06:30 +0000

    规范代码及命名

--- helper/XML.php
+++ helper/XML.php
@@ -1,393 +1,166 @@
 <?
-
 /**
-
  * XML生成类
-
  * 
-
- * @package baseClass
-
- * @version 1.0
-
+ * @package helper
  */
-
-
-
 class helper_XML {
-
-	/***************************************************************************
-
-	* 函数名:			makeInfoXML($arrayList=null, $encoding='UTF-8')
-
-	* 功能:				生成XML
-
-	* 版本:				1.0
-
-	* 日期:				2006-03-03
-
-	* 输入:				$arrayList : 输入的内容数组
-
-	                    $total: 总数
-
-	                    $checkBox： checkBox所要对应的值
-
-	                    $arrayElements： element数组
-
-	                    $encoding: 编码
-
-	* 输出:				
-
-	***************************************************************************/
-
-	function makeInfoXML($arrayList=null, $arrayElements=null, $encoding='UTF-8') {
-
-		$xml = '<?xml version="1.0" encoding="'.$encoding.'"?>';
-
-		$xml .= "<jobui>";
-
-		if(!empty($arrayElements)) {
-
-			$xml.="<elements>";
-
-			while (list($key, $value) = each($arrayElements)) {
-
-				 if(is_numeric($value)) {
-
-			        $xml.="<element elementID=\"".$key."\">".$value."</element>";
-
-			     } else {
-
-			        $xml.="<element elementID=\"".$key."\"><![CDATA[".$value."]]></element>";
-
-			     }
-
-			}
-
-			$xml.="</elements>";
-
-		}
-
-		if(!empty($arrayList)){
-
-			$xml .= "<info>";		
-
-			while (list($key, $value) = @each($arrayList)) {
-
-				if(is_numeric($value)) {
-
-					$xml .="<".$key.">".$value."</".$key.">";
-
-				} else {
-
-					 $xml .="<".$key."><![CDATA[".$value."]]></".$key.">";
-
-				}
-
-			}		
-
-			$xml .= "</info>";
-
-		}else{
-
-			$xml .= "<info />";
-
-		}
-
-		$xml .= "</jobui>";
-
-		return $xml;
-
-	}
-
-	
-
-	
-
-	/***************************************************************************
-
-	* 函数名:			makeXML($arrayList=null, $total=0, $checkBox="", $arrayElements=null, $encoding='UTF-8')
-
-	* 功能:				生成XML
-
-	* 版本:				1.0
-
-	* 日期:				2006-03-03
-
-	* 输入:				$arrayList : 输入的内容数组
-
-	                    $total: 总数
-
-	                    $checkBox： checkBox所要对应的值
-
-	                    $arrayElements： element数组
-
-	                    $encoding: 编码
-
-	* 输出:				
-
-	***************************************************************************/
-
-	function makeXML($arrayList=null, $total=0, $checkBox="ID", $arrayElements=null, $encoding='UTF-8') {
-
-		$xml = '<?xml version="1.0" encoding="'.$encoding.'"?>';
-
-		$innerContent=helper_XML::_arrayInfo2xml($arrayList, $total, $checkBox , $arrayElements);
-
-		if(!empty($innerContent)){
-
-			$xml .= "<jobui>";
-
-			$xml .= $innerContent;
-
-			$xml .= "</jobui>";
-
-		}else{
-
-			$xml .= "<jobui />";
-
-		}
-
-		return $xml;
-
-	}
-
-	
-
-	/***************************************************************************
-
-	* 函数名:			_arrayInfo2xml($arrayList, $total, $checkBox, $arrayElements)
-
-	* 功能:				生成XML
-
-	* 版本:				1.0
-
-	* 日期:				2006-03-03
-
-	***************************************************************************/
-
 	/**
-
-	 * 
-
-	 * @access private
-
+	 * 生成XML
+	 *
+	 * @access public
+	 * @param array $arrayList 输入的内容数组
+	 * @param array $arrayElements element数组
+	 * @param array $arrayInfos
+	 * @param string $headMark
+	 * @param string $encoding 编码
+	 * @return string
 	 */
-
-	function _arrayInfo2xml($arrayList, $total, $checkBox , $arrayElements) {
-
-		$xml = "";
-
-		$checkboxB = false;
-
-		if(!empty($arrayElements)) {
-
-			$xml.="<elements>";
-
-			while (list($key, $value) = each($arrayElements)) {
-
-				 if(is_int($value)) {
-
-			        $xml.="<element elementID=\"".$key."\">".$value."</element>";
-
-			     } else {
-
-			        $xml.="<element elementID=\"".$key."\"><![CDATA[".$value."]]></element>";
-
-			     }
-
-			}
-
-			$xml.="</elements>";
-
+	public function makeXML($arrayList = null, $arrayElements = null, $arrayInfos = null, $headMark = "E-FW", $encoding="UTF-8") {
+		$xml = "<?xml version=\"1.0\" encoding=\"".$encoding."\"?>";
+		$xml.= "<".$headMark.">";
+		
+		if(is_array($arrayElements)) {
+			$xml.= self::getElements($arrayElements);
 		}
 
+		if(is_array($arrayInfos)){
+			$xml.= self::getInfos($arrayInfos);
+		}
 		
+		if(is_array($arrayList)){
+			$xml.= self::getList($arrayList);
+		}
+
+		$xml.= "</".$headMark.">";
+
+		return $xml;
+	}
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @access private
+	 * @param array $dataArray
+	 * @return str
+	 */
+	private function getElements ($dataArray = null) {
+		if (is_array($dataArray)) {
+			$xml.= "<elements>";
+			foreach($dataArray as $key => $value){
+				 if(is_numeric($value)) {
+			        $xml.= "<element elementID=\"".$key."\">".$value."</element>";
+			     } 
+			     else {
+			        $xml.= "<element elementID=\"".$key."\"><![CDATA[".$value."]]></element>";
+			     }
+			}
+			$xml.= "</elements>";
+		}
+		
+		return $xml;
+	}
+	
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @access private
+	 * @param array $dataArray
+	 * @return str
+	 */
+	private function getInfos ($dataArray = null) {
+		if (is_array($dataArray)) {
+			$xml.= "<info>";
+			foreach($dataArray as $key => $value){
+				if(is_numeric($value)) {
+					$xml.= "<".$key.">".$value."</".$key.">";
+				} 
+				else {
+					 $xml.= "<".$key."><![CDATA[".$value."]]></".$key.">";
+				}
+			}		
+			$xml.= "</info>";
+		}
+		
+		return $xml;
+	}
+
+
+	private function getList ($dataArray = null) {
+		$arrayList 	= $dataArray["list"];
+		$total 		= $dataArray["total"];
 
 		if(!empty($arrayList)) {
+			$total 	= empty($total) ? 0 : $total;
+			$xml 	= "<list total=\"".$total."\">";
 
-			$total=empty($total)?0:$total;
-
-
-
-			$checkBoxList = "";
-
-
-
-			$xml.="<list total=\"".$total."\">";
-
-			if(!empty($arrayList)) {			
-
-				while (list($key, $value) = @each($arrayList)) {		
-
-					$xmlList = "";		
-
-					while (list($key2, $value2) = @each($value)) {
-
-						 if($checkBox == $key2) {						     
-
-							 $checkBoxValue = $value2;
-
-						 }
-
-						if($key2 == "ID") {						
-
+			foreach($arrayList as $value){
+				$xmlList 	= "";
+				foreach($value as $key2 => $value2){
+					switch (strtolower($key2)) {
+						case "id":
 							$ID = $value2;
-
-						} else {						
-
-							if($value2 == "" and $value2 != "0") {
-
-								$value2 = "&nbsp;";
-
-							} 						
-
-							if(strtolower($key2) != "checkbox") {
-
-								 if(is_numeric($value)) {
-
-									 $xmlList .="<".$key2.">".$value2."</".$key2.">";
-
-								 } else {
-
-									 $xmlList .= "<".$key2."><![CDATA[".$value2."]]></".$key2.">";
-
-								 }
-
-							} else {
-
-								if(strtolower($key2) == "checkbox") {
-
-									$checkboxB = true;
-
-								}
-
+						
+							break;
+						
+						default:
+							if(is_numeric($value2)) {
+								$xmlList .="<".$key2.">".$value2."</".$key2.">";
+							} 
+							else {
+								$xmlList .= "<".$key2."><![CDATA[".$value2."]]></".$key2.">";
 							}
-
-						}
-
 					}
+				}
 
-					$xml.="<item ID=\"".$ID."\">";
-
-					if($checkboxB) {
-
-						$checkBoxList ="<checkbox><![CDATA[".$checkBoxValue."]]></checkbox>";
-
-					}
-
-					$xml.= $checkBoxList.$xmlList;
-
-					$xml.="</item>";
-
-				}			
-
+				$xml.= "<item ID=\"".$ID."\">";
+				$xml.= $xmlList;
+				$xml.= "</item>";
 			}
-
-			$xml.="</list>";
-
-		}else{
-
-			$total=empty($total)?0:$total;
-
-			$xml.='<list total="'.$total.'" />';
-
+			$xml.= "</list>";
+		}
+		else{
+			$xml = "<list total=\"0\" />";
 		}
 
-
-
 		return $xml;
+	}
 
+
+	public function fileToArray($fileName) {
+		return self::objectToArray(simplexml_load_file($fileName));
 	}
 
 	
-
-	/***************************************************************************
-
-	* 函数名:			xmlFileToArray($fileName)
-
-	* 功能:				文件转成数组
-
-	* 版本:				1.0
-
-	* 日期:				2006-03-03
-
-	***************************************************************************/
-
-	function xmlFileToArray($fileName) {
-
-		return helper_XML::object2array(simplexml_load_file($fileName));
-
+	public function stringToArray($string) {
+		return self::objectToArray(simplexml_load_string($string));
 	}
 
 	
-
-	/***************************************************************************
-
-	* 函数名:			xmlStringToArray($string)
-
-	* 功能:				内容转成数组
-
-	* 版本:				1.0
-
-	* 日期:				2006-03-03
-
-	***************************************************************************/
-
-	function xmlStringToArray($string) {
-
-		return helper_XML::object2array(simplexml_load_string($string));
-
-	}
-
-	
-
-	/***************************************************************************
-
-	* 函数名:			object2array($object)
-
-	* 功能:				对象转成数组
-
-	* 版本:				1.0
-
-	* 日期:				2006-03-03
-
-	***************************************************************************/
-
-	function object2array($object){
+	private function objectToArray($object){
 
 	   $return = NULL;	       
 
 	   if(is_array($object)){
-
-	       foreach($object as $key => $value)
-
-	           $return[$key] = helper_XML::object2array($value);
-
-	   } else {
-
+	       foreach($object as $key => $value){
+	           $return[$key] = self::object2array($value);
+	       }
+	   } 
+	   else {
 	       $var = get_object_vars($object);
 
-	           
-
 	       if($var) {
-
-	           foreach($var as $key => $value)
-
-	               $return[$key] = helper_XML::object2array($value);
-
-	       } else {
-
+	           foreach($var as $key => $value){
+	               $return[$key] = self::object2array($value);
+	           }
+	       } 
+	       else {
 	           return trim((string) $object);
-
 	       }
-
-	   }	
+	   }
 
 	   return $return;
-
 	}
-
-
-
 }
