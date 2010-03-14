d61fdaae6a5619833ac5f373a9325f126fcec41f
eason007 <eason007@94e8bf7a-fd2e-11de-a138-4f08880ddf14>
2010-03-14 10:26:08 +0000

    写入组件：文件

---
+++ library/writer/File.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package Writer
+ */
+
+/**
+ * 写入类
+ * 
+ * <pre>
+ * 
+ * </pre>
+ * 
+ * @package Log
+ * @author eason007<eason007@163.com>
+ * @copyright Copyright (c) 2007-2010 eason007<eason007@163.com>
+ * @version 1.0.0.20100314
+ */
+class Writer_File {
+    protected $_stream = null;
+
+    function __construct ($streamOrUrl) {
+		if ( is_resource($streamOrUrl) and 
+			get_resource_type($streamOrUrl) == 'stream'
+		) {
+            $this->_stream = $streamOrUrl;
+        } else {
+            $this->_stream = @fopen($streamOrUrl, 'a', false);
+        }
+    }
+    
+	public function close () {
+   		if (is_resource($this->_stream)) {
+            fclose($this->_stream);
+        }
+    }
+    
+    public function write ($value) {
+    	if (is_array($value)){
+    		$line = '';
+    		foreach ($value as $k => $v) {
+    			$line.= $k.':'.$v.' | ';
+    		}
+    		$line.= PHP_EOL;
+    	}
+    	else {
+    		$line = (string) $value; 
+    	} 
+    	
+    	if ($this->_stream){
+    		@fwrite($this->_stream, $line);
+    	}
+    }
+}
+?>
